46a2128121e1d956bd94ae3546d92b97925ac876
Roshan Prakash Raut <roshanpk.raut@gmail.com>
2021-05-16 18:25:42 +0530

    deloyed a smart contract.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -11,7 +11,12 @@ const Web3 = require('web3');
 //creating web3 instance
 const web3 = new Web3(ganache.provider());
 
+//creating interface and bytecode for compiling sol code
+const { interface, bytecode } = require('../compile');
+
 let accounts;
+let inbox;
+
 beforeEach(async () => {    
     //Note: every function in web3 library is async ie it's going to return some
     //promises
@@ -19,13 +24,16 @@ beforeEach(async () => {
     //Get a list of all accounts
     accounts = await web3.eth.getAccounts();
         
-    //Use one of those accounts to deploy 
-    //the contract
+    //Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface)) //Teaches web3 about what methods an inbox contrcat has
+        .deploy({ data: bytecode, arguments: ['Hi there! This is Roshan'] }) //tells web3 that we want to deploy a new copy of this contract
+        .send({ from: accounts[0], gas: '1000000' });//instructs web3 to send out a transaction that creates this contract
+
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     });
 });
 
